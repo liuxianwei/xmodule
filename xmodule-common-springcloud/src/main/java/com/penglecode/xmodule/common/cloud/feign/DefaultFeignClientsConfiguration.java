3944ba03b17c7545190abb5b48bc32b6a273dc1d
pengp <pengp@certusnet.com.cn>
2019-06-14 14:04:53 +0800

    commit

--- xmodule-common-springcloud/src/main/java/com/penglecode/xmodule/common/cloud/feign/DefaultFeignClientsConfiguration.java
+++ xmodule-common-springcloud/src/main/java/com/penglecode/xmodule/common/cloud/feign/DefaultFeignClientsConfiguration.java
@@ -3,11 +3,15 @@ package com.penglecode.xmodule.common.cloud.feign;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.DefaultHystrixFallbackFactory;
 import org.springframework.cloud.openfeign.FeignFormatterRegistrar;
 import org.springframework.cloud.openfeign.HystrixFallbackConfiguration;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -19,6 +23,8 @@ import com.penglecode.xmodule.common.boot.config.AbstractSpringConfiguration;
 
 import feign.Feign;
 import feign.Logger;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
 
 /**
  * 默认的Feign自定义配置
@@ -33,11 +39,22 @@ import feign.Logger;
 @Import({HystrixFallbackConfiguration.class})
 public class DefaultFeignClientsConfiguration extends AbstractSpringConfiguration implements WebMvcConfigurer {
 
+	@Autowired
+	private ObjectFactory<HttpMessageConverters> messageConverters;
+	
 	@Bean
 	public Logger.Level feignLoggerLevel(){
 		return  Logger.Level.FULL;
 	}
 	
+	/**
+	 * 实现feign支持form表单提交
+	 */
+    @Bean
+    public Encoder feignEncoder() {
+    	return new SpringFormEncoder(new SpringEncoder(messageConverters));
+    }
+    
 	@Bean
 	@Scope("prototype")
 	@ConditionalOnMissingBean
--- xmodule-examples-springmvc/src/main/java/com/penglecode/xmodule/upms/events/ApplicationStartedEventListener.java
+++ xmodule-examples-springmvc/src/main/java/com/penglecode/xmodule/upms/events/ApplicationStartedEventListener.java
@@ -1,15 +1,28 @@
 package com.penglecode.xmodule.upms.events;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ApplicationStartedEventListener implements ApplicationListener<ApplicationStartedEvent> {
+public class ApplicationStartedEventListener implements ApplicationListener<ApplicationStartedEvent>, ApplicationContextAware {
 
+	private ApplicationContext applicationContext;
+	
 	@Override
 	public void onApplicationEvent(ApplicationStartedEvent event) {
-		System.out.println(">>> ApplicationStartedEvent = " + event);
+		System.out.println(">>> ApplicationStartedEvent1 = " + event);
+		if(event.getApplicationContext().equals(applicationContext)) {
+			System.out.println(">>> ApplicationStartedEvent2 = " + event);
+		}
 	}
 
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+	
 }
